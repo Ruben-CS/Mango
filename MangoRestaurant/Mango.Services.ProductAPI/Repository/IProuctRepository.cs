e87e36f60e24f5d8622d14767b6e6746bca046d6
Ruben Ferreira <rubend192000@gmail.com>
2023-02-13 17:16:36 -0400

    Configured Product Repository Interface

--- MangoRestaurant/Mango.Services.ProductAPI/Repository/IProuctRepository.cs
+++ MangoRestaurant/Mango.Services.ProductAPI/Repository/IProuctRepository.cs
@@ -1,6 +1,12 @@
+using Mango.Services.ProductAPI.Models.Dto;
+
 namespace Mango.Services.ProductAPI.Repository;
 
-public class IProuctRepository
+public interface IProuctRepository
 {
-    
+    public Task<IEnumerable<ProductDto>> GetProducts();
+    public Task<ProductDto>              GetProduct(int productId);
+
+    public Task<ProductDto> CreateUpdateDto(ProductDto productDto);
+    public Task<bool>       DeleteProduct(int productId);
 }
