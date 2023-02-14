3cb3cbf74ffed5865f208a227aa10537119becd9
Ruben Ferreira <rubend192000@gmail.com>
2023-02-14 18:00:10 -0400

    Seeded the database

--- MangoRestaurant/Mango.Services.ProductAPI/DbContexts/ApplicationDbContext.cs
+++ MangoRestaurant/Mango.Services.ProductAPI/DbContexts/ApplicationDbContext.cs
@@ -10,4 +10,37 @@ public class ApplicationDbContext : DbContext
     }
 
     public DbSet<Product> Products { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Product>().HasData(new Product
+        {
+            Id = 1,
+            Name = "Samosa",
+            Price = 15,
+            Description = "Lorem",
+            ImageUrl = string.Empty,
+            CategoryName = "Appetizer"
+        });
+        modelBuilder.Entity<Product>().HasData(new Product
+        {
+            Id           = 2,
+            Name         = "Paneer Tikka",
+            Price        = 13.99,
+            Description  = "Ipsum",
+            ImageUrl     = string.Empty,
+            CategoryName = "Appetizer"
+        });
+        modelBuilder.Entity<Product>().HasData(new Product
+        {
+            Id           = 3,
+            Name         = "Sweet Pie",
+            Price        = 10.99,
+            Description  = "Dolor",
+            ImageUrl     = string.Empty,
+            CategoryName = "Entree"
+        });
+    }
 }
